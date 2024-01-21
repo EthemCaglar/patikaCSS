cb6998b06a63436934dce095b038abe7fdfbe37c
Ethem Çağlar <caglarethem@gmail.com>
2024-01-21 23:20:04 +0300

    ödev 2 updated

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test{
+    public static void main(String[] args) {
+        int N = 4;
+        int sonuc=1;
+        for(int i = 0 ; N ; i++){
+            sonuc = sonuc*i;
+        }
+        System.out.print(sonuc);
+    }
+}
